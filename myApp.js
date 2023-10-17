127c366f134326e7198967b142a78a203caa5130
Nephera Branch <nephera@Nepheras-MBP.attlocal.net>
2023-10-17 10:36:19 -0500

    Add dns prefetch control using helmet

--- myApp.js
+++ myApp.js
@@ -16,7 +16,7 @@ app.use(helmet.ieNoOpen());
 const timeInSeconds = ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 
-
+app.use(helmet.dnsPrefetchControl());
 
 
 
